8fc541cee56f54e1f677f1ca80af0868aefa69a6
Justin <Justin>
2017-05-06 11:58:08 +0200

    keypress

--- script.js
+++ script.js
@@ -14,8 +14,27 @@
 
  });
 
+ //  addEventListener("keypress", function(event) {
+ //      if (event.key === 'z') {
+ //          circle.style.backgroundColor = "#" + Pos.x + Pos.y;
+ //          circle.style.top = Math.random() * 82 + "vh";
+ //          circle.style.left = Math.random() * 82 + "vw";
+ //          point += Math.round(300 + (300 * (combo * (combo * 0.005))));
+ //          pts.textContent = point.toString();
+ //          combo++;
+ //          circle.textContent = combo.toString();
+ //      }
+ //      if (event.key === 'e') {
+ //          circle.style.backgroundColor = "#" + Pos.x + Pos.y;
+ //          circle.style.top = Math.random() * 82 + "vh";
+ //          circle.style.left = Math.random() * 82 + "vw";
+ //          point += Math.round(300 + (300 * (combo * (combo * 0.005))));
+ //          pts.textContent = point.toString();
+ //          combo++;
+ //          circle.textContent = combo.toString();
+ //      }
+ //  });
  if (circle.addEventListener("click", function() {
-
          circle.style.backgroundColor = "#" + Pos.x + Pos.y;
          circle.style.top = Math.random() * 82 + "vh";
          circle.style.left = Math.random() * 82 + "vw";
