904e660e8c56a08f8a54d3340272e6903fab8886
Justin <Justin>
2017-05-06 13:02:04 +0200

    added timer

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
  let Pos = {};
  let pts = document.querySelector("#score");
  let combo = 0;
+ let timer = 1000;
  document.body.addEventListener("mousemove", function(event) {
      let img = document.querySelector("#image");
      Pos.y = event.pageY;
@@ -34,24 +35,35 @@
  //          circle.textContent = combo.toString();
  //      }
  //  });
- if (circle.addEventListener("click", function() {
-         circle.style.backgroundColor = "#" + Pos.x + Pos.y;
-         circle.style.top = Math.random() * 82 + "vh";
-         circle.style.left = Math.random() * 82 + "vw";
-         point += Math.round(300 + (300 * (combo * (combo * 0.005))));
-         pts.textContent = point.toString();
-         combo++;
-         circle.textContent = combo.toString();
 
-     })) {
 
- } else {
-     // let timer = setInterval(function() {
+
+ function miss() {
      combo = 0;
      circle.textContent = combo.toString();
      circle.style.top = Math.random() * 82 + "vh";
      circle.style.left = Math.random() * 82 + "vw";
-
-     // }, 1000);
-
+     timer = 1000;
  }
+
+
+ let timeout;
+
+ function timeUp() {
+     timeout = setTimeout(miss, timer);
+ }
+
+ setInterval(timeUp, 1000);
+
+ circle.addEventListener("click", function() {
+     timer = 700;
+     circle.style.backgroundColor = "#" + Pos.x + Pos.y;
+     circle.style.top = Math.random() * 82 + "vh";
+     circle.style.left = Math.random() * 82 + "vw";
+     point += Math.round(300 + (300 * (combo * (combo * 0.005))));
+     pts.textContent = point.toString();
+     combo++;
+     circle.textContent = combo.toString();
+     clearTimeout(timeout);
+
+ })
