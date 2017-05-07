4c32ae6c9aafd05056b96c284d90bdf7d7becc57
Justin <Justin>
2017-05-07 12:54:10 +0200

    combo color+difficulty

--- script.js
+++ script.js
@@ -18,6 +18,8 @@
 
  //  addEventListener("keypress", function(event) {
  //      if (event.key === 'z') {
+ //          timer = 700;
+ //          timer2 = 1000;
  //          circle.style.backgroundColor = "#" + Pos.x + Pos.y;
  //          circle.style.top = Math.random() * 82 + "vh";
  //          circle.style.left = Math.random() * 82 + "vw";
@@ -25,8 +27,11 @@
  //          pts.textContent = point.toString();
  //          combo++;
  //          circle.textContent = combo.toString();
+ //          clearTimeout(timeout);
  //      }
  //      if (event.key === 'e') {
+ //          timer = 700;
+ //          timer2 = 1000;
  //          circle.style.backgroundColor = "#" + Pos.x + Pos.y;
  //          circle.style.top = Math.random() * 82 + "vh";
  //          circle.style.left = Math.random() * 82 + "vw";
@@ -34,20 +39,21 @@
  //          pts.textContent = point.toString();
  //          combo++;
  //          circle.textContent = combo.toString();
+ //          clearTimeout(timeout);
  //      }
  //  });
-
-
 
  function miss() {
      combo = 0;
      circle.textContent = combo.toString();
+     circle.style.color = "white";
+     circle.style.textShadow = "none";
+     circle.style.borderColor = "white";
      circle.style.top = Math.random() * 82 + "vh";
      circle.style.left = Math.random() * 82 + "vw";
      timer = 1000;
      timer2 = 2000;
  }
-
 
  let timeout;
 
@@ -67,6 +73,32 @@
      pts.textContent = point.toString();
      combo++;
      circle.textContent = combo.toString();
-     clearTimeout(timeout);
+     if (combo >= 50) {
+         timer = 650;
+         circle.style.color = "#d60816";
+         circle.style.textShadow = "1px 1px white";
+         circle.style.borderColor = "white white white #d60816";
+     }
 
+     if (combo >= 100) {
+         timer = 575;
+         circle.style.color = "#1981a0";
+         circle.style.textShadow = "1px 1px white";
+         circle.style.borderColor = "white #1981a0";
+     }
+     if (combo >= 200) {
+         timer = 475;
+         circle.style.color = "#1bc646";
+         circle.style.textShadow = "1px 1px white";
+         circle.style.borderColor = "#1bc646 #1bc646 white #1bc646";
+     }
+
+     if (combo >= 500) {
+         timer = 350;
+         circle.style.color = "#c40f88";
+         circle.style.textShadow = "1px 1px white";
+         circle.style.borderColor = "#c40f88";
+         circle.style.backgroundColor = "white";
+     }
+     clearTimeout(timeout);
  })
